081011cc1ce110950f25dd022748217b7692ffc6
dvir <dvirs120@gmail.com>
2020-06-08 17:55:27 +0300

    g 96

--- FootSoldier.hpp
+++ FootSoldier.hpp
@@ -17,16 +17,17 @@ namespace WarGame
 
             void MAction(std::vector<std::vector<Soldier*>> &board, std::pair<int,int> dest_location) override
             {
-                Soldier** close = closest(board, board[dest_location.first][dest_location.second]->player, dest_location);
-                (*close)->hp = (*close)->hp - this->dpa; // Shoot!
+                auto close = closest(board, board[dest_location.first][dest_location.second]->player, dest_location);
+                board[close.first][close.second]->hp = board[close.first][close.second]->hp - this->dpa; // Shoot!
+                // (*close)->hp = (*close)->hp - this->dpa; 
             }
 
         private:
-            Soldier** closest(std::vector<std::vector<Soldier*>> &board, int playernum, pair<int, int> loc1)
+            pair<int, int> closest(std::vector<std::vector<Soldier*>> &board, int playernum, pair<int, int> loc1)
             {
                 int min = distance({0, 0}, {board.size(), board[0].size()})+1; // Bigger than the diagnal
                 int temp;
-                Soldier** sol;
+                pair<int, int> sol;
                 for(int i= 0; i < board.size(); ++i)
                 {
                     for(int j = 0; j < board[0].size(); ++j)
@@ -37,7 +38,8 @@ namespace WarGame
                             if(temp <= min && board[i][j]->player != playernum)
                             {
                                 min = temp;
-                                *sol = board[i][j];
+                                sol.first = i;
+                                sol.second = j;
                             }
                         } 
                     }  
